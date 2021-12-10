afbdd79dd90039f1be65814a4dd647178ca89867
Artem Gribanov <gribaart@fel.cvut.cz>
2021-12-10 11:30:32 +0100

    Update coin.sol

--- coin.sol
+++ coin.sol
@@ -2,7 +2,15 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VseToken is ERC20 {
-    constructor() ERC20("VseToken", "VSET") {}
+contract VseCoin is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("VseCoin", "VSC") {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
